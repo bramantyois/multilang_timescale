bf334b69ed291b5aa06520f15b2e72b731afe3fc
bramantyois <ibrahim.bramantyo@gmail.com>
2024-04-07 15:53:17 +0200

    bert zh bert added

--- scripts/00_encoding_model/05_mkr_bling_stepwise.py
+++ scripts/00_encoding_model/05_mkr_bling_stepwise.py
@@ -23,42 +23,50 @@ def train(
     trainer.train()
     trainer.refit_and_evaluate()
 
-subject_id = "GFW"
+subject_ids = [
+    # 'TYE', 
+    # 'GFW', 
+    'COL', ]
 
-feature_config_paths = [ 
-    # ".temp/config/bling/feature/mBERT_all_untrimmed_timescale_en_COL.json",
-    # ".temp/config/bling/feature/mBERT_all_untrimmed_timescale_zh_COL.json",
-    # ".temp/config/bling/feature/COL/mBERT_all_untrimmed_timescale_stepwise_en.json",
-    # ".temp/config/bling/feature/COL/mBERT_all_untrimmed_timescale_stepwise_zh.json"
-    f".temp/config/bling/feature/{subject_id}/mBERT_all_untrimmed_timescale_stepwise_en.json",
-    f".temp/config/bling/feature/{subject_id}/mBERT_all_untrimmed_timescale_stepwise_zh.json"
-]
+for subject_id in subject_ids:
+    feature_config_paths = [ 
+        # ".temp/config/bling/feature/mBERT_all_untrimmed_timescale_en_COL.json",
+        # ".temp/config/bling/feature/mBERT_all_untrimmed_timescale_zh_COL.json",
+        # ".temp/config/bling/feature/COL/mBERT_all_untrimmed_timescale_stepwise_en.json",
+        # ".temp/config/bling/feature/COL/mBERT_all_untrimmed_timescale_stepwise_zh.json"
+        # f".temp/config/bling/feature/{subject_id}/mBERT_all_untrimmed_timescale_stepwise_en.json",
+        # f".temp/config/bling/feature/{subject_id}/mBERT_all_untrimmed_timescale_stepwise_zh.json"
+        f".temp/config/bling/feature/{subject_id}/BERT_all_untrimmed_timescale_stepwise_en.json",
+        f".temp/config/bling/feature/{subject_id}/zhBERT_all_untrimmed_timescale_stepwise_zh.json"
+    ]
 
-sub_config_paths = [
-    # ".temp/config/bling/subject/COL_en.json",
-    # ".temp/config/bling/subject/COL_zh.json",
-    f".temp/config/bling/subject/{subject_id}_en.json",
-    f".temp/config/bling/subject/{subject_id}_zh.json",
-]
+    sub_config_paths = [
+        # ".temp/config/bling/subject/COL_en.json",
+        # ".temp/config/bling/subject/COL_zh.json",
+        f".temp/config/bling/subject/{subject_id}_en.json",
+        f".temp/config/bling/subject/{subject_id}_zh.json",
+    ]
 
-train_config_paths = [
-    # ".temp/config/bling/train/stepwise/col_en_timescale.json",
-    # ".temp/config/bling/train/stepwise/col_zh_timescale.json",
-    f".temp/config/bling/train/stepwise/{subject_id.lower()}_en_timescale.json",
-    f".temp/config/bling/train/stepwise/{subject_id.lower()}_zh_timescale.json",
-]
+    train_config_paths = [
+        # ".temp/config/bling/train/stepwise/col_en_timescale.json",
+        # ".temp/config/bling/train/stepwise/col_zh_timescale.json",
+        f".temp/config/bling/train/stepwise/{subject_id.lower()}_en_timescale.json",
+        f".temp/config/bling/train/stepwise/{subject_id.lower()}_zh_timescale.json",
+    ]
 
-# # COL training
-train(
-    config_feature_path=feature_config_paths[0],
-    config_subject_path=sub_config_paths[0],
-    config_train_path=train_config_paths[0],
-)
+    print("sub_config_paths", sub_config_paths)
 
-# # ZH 
-train(
-    config_feature_path=feature_config_paths[1],
-    config_subject_path=sub_config_paths[1],
-    config_train_path=train_config_paths[1],
-)
+    ## EN
+    train(
+        config_feature_path=feature_config_paths[0],
+        config_subject_path=sub_config_paths[0],
+        config_train_path=train_config_paths[0],
+    )
+
+    ## ZH 
+    train(
+        config_feature_path=feature_config_paths[1],
+        config_subject_path=sub_config_paths[1],
+        config_train_path=train_config_paths[1],
+    )
 
